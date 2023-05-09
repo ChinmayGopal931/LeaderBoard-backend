4d940d209695e0a8992fab58515048c7825257f9
ChinmayGopal931 <chinmayg015@gmail.com>
2023-05-09 04:19:22 -0700

    fix: add post request

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 const fs = require("fs");
 
-let data = {};
+let data: any = {};
 
 app.use(express.json());
 
@@ -13,6 +13,13 @@ app.use((req: any, res: any, next: any) => {
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
+});
+
+app.options("/api/postData", (req: any, res: any) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.send();
 });
 
 // Read data from the file on startup
@@ -32,13 +39,15 @@ app.get("/api/data", (req: any, res: any) => {
   res.json(data);
 });
 
-app.post("/api/data", (req: any, res: any) => {
+app.post("/api/postData", (req: any, res: any) => {
   const newData = req.body;
-  data = { ...data, ...newData };
+  data.data.push(newData);
+
   res.json(data);
   fs.writeFileSync("data.json", JSON.stringify(data));
 });
 
 app.listen(port, () => {
+  console.log(data);
   console.log(`Server listening at http://localhost:${port}`);
 });
